2665056ddb23f1db2b8cc5f33a5fcaba6b69c764
Michael Madden <mikemadden42@icloud.com>
2021-01-20 18:23:58 -0600

    Remove comment about broken cask installs.

--- setup-casks.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh -x
-
-brew install --cask brave-browser docker firefox google-chrome intel-power-gadget iterm2 kindle macpass messenger postman rectangle signal slack spotify sublime-text visual-studio-code zoom

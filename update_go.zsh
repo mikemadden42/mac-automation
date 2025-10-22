263bc1d7ac17c5aaec0d573820b6d401de1473a9
Michael Madden <mikemadden42@icloud.com>
2025-10-22 11:44:46 -0500

    Update for v3 of task.

--- update_go.zsh
+++ update_go.zsh
@@ -16,7 +16,7 @@ projects=(
 	github.com/dimkouv/massivedl/cmd/massivedl
 	github.com/dundee/gdu
 	github.com/elastic/go-licenser
-	github.com/go-task/task
+	github.com/go-task/task/v3/cmd/task
 	github.com/goreleaser/goreleaser/v2
 	github.com/hhatto/gocloc/cmd/gocloc
 	github.com/icholy/gomajor
